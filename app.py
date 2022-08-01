fc6f16fc4090259db91315a1ef76ae015a0d7d60
nehakhatri2021 <110082217+nehakhatri2021@users.noreply.github.com>
2022-08-01 15:25:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,17 +14,7 @@ def home():
   
 @app.route('/predict',methods=['GET'])
 def predict():
-    
-    
-    '''
-    For rendering results on HTML GUI
-    '''
-    exp = float(request.args.get('exp'))
-    
-    prediction = model.predict([[exp]])
-    
-        
-    return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
-
-
+  exp = float(request.args.get('exp'))
+  prediction = model.predict([[exp]])
+  return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
 app.run(debug=True)
