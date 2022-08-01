18902f590d00dc7ebdd12b677c5b7f66e3734044
nehakhatri2021 <110082217+nehakhatri2021@users.noreply.github.com>
2022-08-01 15:42:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
-
 app = Flask(__name__)
 model = pickle.load(open('linearregression.pkl','rb')) 
-
-
 @app.route('/')
 def home():
-  
-    return render_template("index.html")
-  
+  return render_template("index.html")
 @app.route('/predict',methods=['GET'])
 def predict():
   exp = float(request.args.get('exp'))
